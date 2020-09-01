a723f502ebaf5d01520341b0709259dee10ff683
Svetlana <61006704+Kaorry@users.noreply.github.com>
2020-09-01 21:12:19 +0300

    Добавляет исправления

--- source/js/script.js
+++ source/js/script.js
@@ -5,29 +5,24 @@ var slidesArray = document.querySelectorAll(".example__slide");
 var exampleState = document.querySelector(".example__state");
 var exampleStateList = ["example__state--before", "example__state--after"];
 
-if (buttonsArray.length === 2 && buttonsArray.length === slidesArray.length)
-{
-  var clearCurrent = function()
-  {
+if (buttonsArray.length === 2 && buttonsArray.length === slidesArray.length) {
+  var clearCurrent = function () {
     slidesArray[0].classList.remove("example__slide--current");
     slidesArray[1].classList.remove("example__slide--current");
     exampleState.classList.remove(exampleStateList[0], exampleStateList[1]);
   };
 
-  var setCurrent = function(index)
-  {
+  var setCurrent = function (index) {
     clearCurrent();
     slidesArray[index].classList.add("example__slide--current");
     exampleState.classList.add(exampleStateList[index]);
   }
 
-  buttonsArray[0].addEventListener("click", function()
-  {
+  buttonsArray[0].addEventListener("click", function () {
     setCurrent(0);
   });
 
-  buttonsArray[1].addEventListener("click", function()
-  {
+  buttonsArray[1].addEventListener("click", function () {
     setCurrent(1);
   });
 }
@@ -36,10 +31,8 @@ if (buttonsArray.length === 2 && buttonsArray.length === slidesArray.length)
 var buttonMenu = document.querySelector(".main-nav__toggle");
 var menuToggle = document.querySelector(".main-nav__list");
 
-if (buttonMenu && menuToggle)
-{
-  buttonMenu.addEventListener("click", function()
-  {
+if (buttonMenu && menuToggle) {
+  buttonMenu.addEventListener("click", function () {
     buttonMenu.classList.toggle("main-nav__toggle--close");
     menuToggle.classList.toggle("main-nav__list--hide");
   })
